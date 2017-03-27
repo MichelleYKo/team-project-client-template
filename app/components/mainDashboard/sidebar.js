0d0a1b25d05d4b6e67aa7cb05ef176aa0cf072a4
Sam McGuire <sam.holmes.mcg@gmail.com>
2017-03-26 20:10:45 -0400

    added more mainDashboard and mainDashboard music components

--- app/components/mainDashboard/sidebar.js
+++ app/components/mainDashboard/sidebar.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import SidebarHeader from './mainDashboard/sidebarheader';
-import SidebarList from './mainDashboard/sidebarlist';
-import SidebarFooter from './mainDashboard/sidebarfooter';
+import SidebarHeader from './sidebarheader.js';
+import SidebarList from './sidebarlist.js';
+import SidebarFooter from './sidebarfooter.js';
 
 export default class Sidebar extends React.Component {
   render() {
     return (
-      <div>
+      <div id="sidebar" className="col-md-2 sidebar">
         <SidebarHeader />
         <hr />
         <SidebarList />

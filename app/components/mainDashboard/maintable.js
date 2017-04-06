f6d4d9f2f7fd19146e5c8928217e95742acb6717
Sam McGuire <sam.holmes.mcg@gmail.com>
2017-04-06 17:13:35 -0400

    Added some server functionality (that may or may not be functional)

--- app/components/mainDashboard/maintable.js
+++ app/components/mainDashboard/maintable.js
@@ -4,6 +4,20 @@ import TableHead from './tablehead'
 
 
 export default class MainTable extends React.Component {
+    constructor(props){
+      super(props);
+      this.state = {
+        contents: []
+      }
+  }
+
+  componentDidMount() {
+    getFeedData(this.props.user, (playlistData) => {
+      this.setState(playlistData);
+    });
+  }
+
+
   render() {
     return (
         <div id="mainPanel" className="main-page col-md-10 col-md-offset-2" style={{height:999}} >
--- app/server.js
+++ app/server.js
@@ -24,6 +24,41 @@ export function addPlaylist(name, description, friendList) {
   addDocument('playlists', playlist);
 }
 
+export function getPlaylistList(user, cb) {
+  // Get the User object with the id "user".
+  var userData = readDocument('users', user);
+  // Get the Feed object for the user.
+  var playlistList = readDocument('playlists', userData.playlistCollection);
+  userData.playlistCollection.map( (playlist) =>
+          readDocument('playlists', playlist)
+    );
+  // Map the Feed's FeedItem references to actual FeedItem objects.
+  // Note: While map takes a callback function as an argument, it is
+  // synchronous, not asynchronous. It calls the callback immediately.
+  
+  // Return FeedData with resolved references.
+  // emulateServerReturn will emulate an asynchronous server operation, which
+  // invokes (calls) the "cb" function some time in the future.
+  emulateServerReturn(playlistList, cb);
+}
+
+function getPlaylistItemSync(feedItemId) {
+  var feedItem = readDocument('feedItems', feedItemId);
+  // Resolve 'like' counter.
+  feedItem.likeCounter =
+    feedItem.likeCounter.map((id) => readDocument('users', id));
+  // Assuming a StatusUpdate. If we had other types of
+  // FeedItems in the DB, we would
+  // need to check the type and have logic for each type.
+  feedItem.contents.author =
+    readDocument('users', feedItem.contents.author);
+  // Resolve comment author.
+  feedItem.comments.forEach((comment) => {
+    comment.author = readDocument('users', comment.author);
+  });
+  return feedItem;
+}
+
 export function calculateVotes(upvotes, downvotes){
   //Take a song and all of the user IDs associated with it
   return upvotes-downvotes;

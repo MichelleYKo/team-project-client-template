42b1bce71098b66efc2b2d24a5f8ddbd5ddc9837
Sam McGuire <sam.holmes.mcg@gmail.com>
2017-04-11 01:28:07 -0400

    click to select song, double click to play song

--- app/components/mainDashboard/tablebody.js
+++ app/components/mainDashboard/tablebody.js
@@ -2,13 +2,24 @@ import React from 'react';
 import TableItem from './tableitem.js'
 
 export default class TableBody extends React.Component {
+  constructor(props) {
+    super(props)
+    this.handleClick = this.handleClick.bind(this);
+    this.state = {
+      activeIndex: null
+    }
+  }
+
+  handleClick(index) {
+    this.setState({activeIndex: index})
+  }
 
   render() {
     return (
       <tbody>
         {this.props.currentPlaylist.playlistItems.map((playlistItem, i) => {
           return (
-            <TableItem key={i} playlistItem={playlistItem} handleSelectSong = {this.props.handleSelectSong}/>
+            <TableItem key={i} id = {i} playlistItem={playlistItem} handleSelectSong={this.props.handleSelectSong} handleSelectItem={this.handleClick} isActive={i == this.state.activeIndex}/>
           )
         })}
       </tbody>
--- app/components/mainDashboard/tableitem.js
+++ app/components/mainDashboard/tableitem.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
 export default class TableItem extends React.Component {
-
   render() {
 
+
     return (
-        <tr href="#"  onDoubleClick={ (e) => this.props.handleSelectSong(e, this.props.playlistItem) }>
+        <tr className = {this.props.isActive ? 'table-selected' : ''} onDoubleClick={ (e) => this.props.handleSelectSong(e, this.props.playlistItem)} onClick = { () => this.props.handleSelectItem(this.props.id)}>
           <td>  {this.props.playlistItem._id} </td>
           <td>  {this.props.playlistItem.title} </td>
           <td>  {this.props.playlistItem.artists[0]} </td>
